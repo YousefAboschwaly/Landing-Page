288dac56384ee26555791553f409a91826a5d309
yousef <yousefshwaly@gmail.com>
2026-01-14 18:17:11 +0200

    Initalize index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 export default function App() {
   return (
-    <div className="w-full h-lvh flex justify-center items-center">App</div>
+    <div className="">
+      
+    </div>
   )
 }
---
+++ src/data/index.js
@@ -0,0 +1,26 @@
+export const NAV_LINKS = [
+  {
+    label: "How it works",
+    href: "/how-it-works",
+  },
+  {
+    label: "All apps",
+    href: "/apps",
+  },
+  {
+    label: "Pricing",
+    href: "/pricing",
+  },
+  {
+    label: "For Teams",
+    href: "/teams",
+  },
+  {
+    label: "Blog",
+    href: "/blog",
+  },
+  {
+    label: "Podcast",
+    href: "/podcast",
+  },
+];
