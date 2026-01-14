f390d50d2af8b1d36d2412961b07f8ad251ad484
yousef <yousefshwaly@gmail.com>
2026-01-15 00:58:49 +0200

    Create Header Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Navbar from "./components/ui/Navbar";
+import { Hero } from "./components/ui/header/HeaderContent";
+import Navbar from "./components/ui/header/Navbar";
 
 export default function App() {
   return (
-    <div >
-        <Navbar/>
+    <div>
+      <Header/>
     </div>
-  )
+  );
 }
---
+++ src/sections/Header.jsx
@@ -0,0 +1,12 @@
+import { HeaderContent } from '../components/ui/header/HeaderContent'
+import Navbar from '../components/ui/header/Navbar'
+
+export default function Header() {
+  return (
+    <>
+      <Navbar />
+      <HeaderContent />
+    
+    </>
+  )
+}
