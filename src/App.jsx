a3add9fb8d2fb0fb11bda33bdd1317677974b292
yousef <yousefshwaly@gmail.com>
2026-01-15 13:10:35 +0200

    Install lucide

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Explore from "./sections/Explore";
+import TestimonialCard from "./sections/Testimonial";
 import Header from "./sections/Header";
-import Setapp from './sections/Setapp';
-
+import Setapp from "./sections/Setapp";
 
 export default function App() {
   return (
     <div>
-      <Header/>
-      <Setapp/>
-      <Explore/>
+      <Header />
+      <Setapp />
+      <div className="bg-[#FEFEFE]  mt-130   relative z-10">
+        <TestimonialCard />
+      </div>
     </div>
   );
 }
